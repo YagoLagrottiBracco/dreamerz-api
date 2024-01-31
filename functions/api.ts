735792df2612982582ba93c50c477e0ee64702ff
Yago Lagrotti Bracco <yago.lagrotti@outlook.com>
2024-01-31 16:44:07 -0300

    fix(all) fixed files to work correctly now on netlify

--- functions/api.ts
+++ functions/api.ts
@@ -1,6 +1,7 @@
 const cors = require("cors")
 import express from "express"
 import serverless from "serverless-http"
+import db from "../src/configs/mongo.config"
 import privateRoutes from "../src/routes/private.routes"
 import publicRoutes from "../src/routes/public.routes"
 
@@ -15,5 +16,8 @@ api.use(express.json())
     )
     .use("/", publicRoutes)
     .use("/dashboard/", privateRoutes)
+    .listen("https://zingy-daifuku-686d5f.netlify.app", async () => {
+        await db()
+    })
 
 module.exports.handler = serverless(api)
